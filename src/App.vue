3327fee26b9179c490b75af2854a02344b4b5654
gutembergborges <gutem.borges@gmail.com>
2020-11-15 17:25:19 -0300

    first changes in main page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/capgemini-logo.svg">
+    <BankTerminal msg="Bank Terminal"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BankTerminal from './components/BankTerminal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BankTerminal
   }
 }
 </script>
---
+++ src/components/BankTerminal.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+    </p>
+    
+    <h3>Developed by Gutemberg Borges in VueJS</h3>
+    <ul>
+      <li><a href="https://github.com/gutembergborges/frontbank" target="_blank" rel="noopener">GitHub</a></li>
+      <li><a href="https://github.com/gutembergborges" target="_blank" rel="noopener">Gutemberg Borges</a></li>
+      <li><a href="https://vuejs.org/" target="_blank" rel="noopener">Vue.js</a></li>
+      <li><a href="https://www.capgemini.com/" target="_blank" rel="noopener">Capgemini</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BankTerminal',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
